e7b12522605642058caa1bf589a4bbd048b26f6f
Jiří Fatka <ntsfka@pm.me>
2025-09-26 10:15:43 +0200

    Added support for Go's nilaway tool.

---
+++ internal/tool/nilaway.go
@@ -0,0 +1,39 @@
+package tool
+
+import (
+	"cdt/internal"
+	"context"
+)
+
+// A NilAway is a tool that wraps golang main tool `nilaway`.
+type NilAway struct {
+	internal.ExecutableTool
+}
+
+// NewNilAway creates a go tool from a custom executable
+func NewNilAway(detect func() *internal.Executable) *NilAway {
+	return &NilAway{
+		internal.MakeExecutableTool(
+			"nilaway",
+			"NilAway",
+			"NilAway is a static analysis tool that seeks to help developers avoid nil panics in production.",
+			internal.Tags{internal.ToolTagGo, internal.ToolTagLint},
+			detect,
+		),
+	}
+}
+
+// DetectNilAway create nilaway tool can be used in the project
+func DetectNilAway(ctx context.Context, environment internal.Environment) *NilAway {
+	return NewNilAway(func() *internal.Executable {
+		return environment.FindExecutable(ctx, "nilaway")
+	})
+}
+
+func (c *NilAway) LintAll(ctx context.Context, options internal.ProjectLinterOptions) error {
+	return c.RunForProject(ctx, options.ProjectInfo, append(options.ExtraArgs, "./..."))
+}
+
+func (c *NilAway) LintFiles(ctx context.Context, options internal.ProjectLinterOptions, modules []string) error {
+	return c.RunForProject(ctx, options.ProjectInfo, append(options.ExtraArgs, modules...))
+}
---
+++ internal/tool/nilaway_test.go
@@ -0,0 +1,73 @@
+package tool
+
+import (
+	"cdt/internal"
+	"cdt/internal/test"
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNilAway_DetectNilAway(t *testing.T) {
+	env := test.NewEnvironment(t)
+	env.OnFindExecutable("nilaway").
+		Return(env.NewExecutable("/bin/tool"))
+
+	tool := DetectNilAway(context.Background(), env)
+	assert.NotNil(t, tool)
+	assert.Equal(t, "nilaway", tool.Id())
+	assert.True(t, tool.IsAvailable())
+
+	if executable := tool.Executable(); assert.NotNil(t, executable) {
+		assert.Equal(t, "/bin/tool", executable.Path)
+	}
+
+	env.AssertExpectations(t)
+}
+
+func TestNilAway_DetectNilAway_NotFound(t *testing.T) {
+	env := test.NewEnvironment(t)
+	env.OnFindExecutable("nilaway").
+		Return(nil)
+
+	tool := DetectNilAway(context.Background(), env)
+	assert.NotNil(t, tool)
+	assert.Equal(t, "nilaway", tool.Id())
+	assert.False(t, tool.IsAvailable())
+	assert.Nil(t, tool.Executable())
+
+	env.AssertExpectations(t)
+}
+
+func TestNilAway_NilAway_LintAll(t *testing.T) {
+	exec := test.NewExecutable(t)
+
+	tool := NewNilAway(exec.LazyExecutable("lint"))
+
+	info := internal.ProjectInfo{Directory: "."}
+
+	exec.OnRun("lint", []string{"./..."}).
+		Return(nil)
+
+	err := tool.LintAll(context.Background(), internal.ProjectLinterOptions{ProjectInfo: info})
+	assert.NoError(t, err)
+
+	exec.AssertExpectations(t)
+}
+
+func TestNilAway_NilAway_Lint(t *testing.T) {
+	exec := test.NewExecutable(t)
+
+	tool := NewNilAway(exec.LazyExecutable("lint"))
+
+	info := internal.ProjectInfo{Directory: "."}
+
+	exec.OnRun("lint", []string{"mod1"}).
+		Return(nil)
+
+	err := tool.LintFiles(context.Background(), internal.ProjectLinterOptions{ProjectInfo: info}, []string{"mod1"})
+	assert.NoError(t, err)
+
+	exec.AssertExpectations(t)
+}
